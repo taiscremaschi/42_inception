8c16aaf37a9d1aa1bd32de3ac174f2d4c2be3e3a
taiscremaschi <taiscremaschi@gmail.com>
2024-08-07 11:59:15 +0100

    wp

--- srcs/requeriments/mariadb/conf/docker-entrypoint.sh
+++ srcs/requeriments/mariadb/conf/docker-entrypoint.sh
@@ -3,13 +3,30 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysqld --initialize-insecure
     service mysql start
 
-    mysql -uroot -e "My data base being created $MYSQL_DATABASE;"
-    mysql -uroot -e "User beig created'$MYSQL_USER'@'%' pass send '$MYSQL_PASSWORD';"
-    mysql -uroot -e "Privileges on $MYSQL_DATABASE.* to user: '$MYSQL_USER'@'%';"
-    mysql -uroot -e "Alter user 'root'@'localhost' root pass '$MYSQL_ROOT_PASSWORD';"
+    mysql -uroot -e "My data base being created $DATABASE;"
+    mysql -uroot -e "User beig created'$MARIA_USER'@'%' pass send '$MARIA_PASSWORD';"
+    mysql -uroot -e "Privileges on $DATABASE.* to user: '$MARIA_USER'@'%';"
+    mysql -uroot -e "Alter user 'root'@'localhost' root pass '$MARIA_ROOT_PASSWORD';"
     mysql -uroot -e "Flush privileges;"
     
     service mysql stop
 fi
 
-exec "$@"
+# exec "$@"
+
+#!/bin/bash
+
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
+#     mysqld --initialize-insecure
+#     service mysql start
+
+#     mysql -uroot -e "CREATE DATABASE ${DATABASE};"
+#     mysql -uroot -e "CREATE USER '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';"
+#     mysql -uroot -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${MARIA_USER}'@'%';"
+#     mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_ROOT_PASSWORD}';"
+#     mysql -uroot -e "FLUSH PRIVILEGES;"
+    
+#     service mysql stop
+# fi
+
+mysqld
---
+++ srcs/requeriments/nginx/conf/docker-entrypoint.sh
@@ -0,0 +1,2 @@
+nginx -g 'daemon off;'
+
