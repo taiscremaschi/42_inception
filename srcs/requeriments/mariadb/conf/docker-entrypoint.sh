bce83f7adb50033460d45cee6a7c5e26cdc1e708
taiscremaschi <taiscremaschi@gmail.com>
2024-08-07 12:39:44 +0100

    fix: mariadb

--- srcs/requeriments/mariadb/conf/docker-entrypoint.sh
+++ srcs/requeriments/mariadb/conf/docker-entrypoint.sh
@@ -1,32 +1,21 @@
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+mkdir -p /var/run/mysqld
+chown mysql:mysql /var/run/mysqld
+
+if [ ! -d "/var/lib/mysql/$DATABASE" ]; then
     mysqld --initialize-insecure
-    service mysql start
 
-    mysql -uroot -e "My data base being created $DATABASE;"
-    mysql -uroot -e "User beig created'$MARIA_USER'@'%' pass send '$MARIA_PASSWORD';"
-    mysql -uroot -e "Privileges on $DATABASE.* to user: '$MARIA_USER'@'%';"
-    mysql -uroot -e "Alter user 'root'@'localhost' root pass '$MARIA_ROOT_PASSWORD';"
-    mysql -uroot -e "Flush privileges;"
+    while ! mysqladmin ping --silent; do
+        sleep 1
+    done
+
+    mysql -uroot -e "CREATE DATABASE ${DATABASE};"
+    mysql -uroot -e "CREATE USER '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${MARIA_USER}'@'%';"
+    mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_ROOT_PASSWORD}';"
+    mysql -uroot -e "FLUSH PRIVILEGES;"
     
-    service mysql stop
+     killall mysqld
 fi
-
-# exec "$@"
-
-#!/bin/bash
-
-# if [ ! -d "/var/lib/mysql/mysql" ]; then
-#     mysqld --initialize-insecure
-#     service mysql start
-
-#     mysql -uroot -e "CREATE DATABASE ${DATABASE};"
-#     mysql -uroot -e "CREATE USER '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';"
-#     mysql -uroot -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${MARIA_USER}'@'%';"
-#     mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_ROOT_PASSWORD}';"
-#     mysql -uroot -e "FLUSH PRIVILEGES;"
-    
-#     service mysql stop
-# fi
 
 mysqld
