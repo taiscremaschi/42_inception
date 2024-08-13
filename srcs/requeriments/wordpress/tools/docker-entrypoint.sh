e5c982a5c3e236db1db8140ac4ec1c9ca136bd23
taiscremaschi <taiscremaschi@gmail.com>
2024-08-13 16:00:19 +0100

    fix: names of volumes

--- srcs/requeriments/wordpress/tools/docker-entrypoint.sh
+++ srcs/requeriments/wordpress/tools/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 sed -i "s/database_name_here/$DATABASE/g" /var/www/html/wp-config.php
 sed -i "s/username_here/$MARIA_USER/g" /var/www/html/wp-config.php
 sed -i "s/password_here/$MARIA_PASSWORD/g" /var/www/html/wp-config.php
-sed -i "s/localhost/mari/g" /var/www/html/wp-config.php
+sed -i "s/localhost/mariadb/g" /var/www/html/wp-config.php
 
 
 php-fpm7.4 --nodaemonize
